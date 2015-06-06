463b8cbfb5779cb8d23feaa7e110a09d5865460e
Jerry Jäppinen <eiskis@gmail.com>
2015-06-06 13:38:57 +0300

    Iteration

--- theme/base/gulpfile.js
+++ theme/base/gulpfile.js
@@ -19,7 +19,7 @@ var config = {
 	cssUrlPrefix: '../',
 	source: {
 		css: [
-			'source/**/*.css'
+			'source/**/*.{css,less}'
 		],
 		js: [
 			'source/**/*.js'
@@ -47,8 +47,15 @@ gulp.task('clear-css', function (cb) {
 
 // Bower components
 gulp.task('css-vendor', function() {
-	return gulp.src(plugins.bowerFiles(['**/*.css']), { base: config.bowerComponentsPath })
+	var files = gulp.src(plugins.bowerFiles(['**/*.css']), { base: config.bowerComponentsPath });
+	return files
 		.pipe(plugins.plumber())
+
+		// Log files
+		.pipe(plugins.if(config.debug, plugins.debug({
+			title: 'File (vendor CSS): '
+		})))
+
 		.pipe(plugins.concat('vendor.css'))
 		.pipe(plugins.if(!config.debug, plugins.minifyCss()))
 		// .pipe(plugins.rename({suffix: '.min'}))
@@ -56,17 +63,40 @@ gulp.task('css-vendor', function() {
 });
 
 // Compile, autoprefix and, minify CSS
-gulp.task('css', ['clear-css', 'css-vendor'], function () {
+gulp.task('css', function () {
 	var files = gulp.src(config.source.css);
 	return files
 		.pipe(plugins.plumber())
+
+		// Log files
+		.pipe(plugins.if(config.debug, plugins.debug({
+			title: 'File (CSS): '
+		})))
+
+		// We want a single file
 		.pipe(plugins.concat('all.css'))
+
+		// Parse all as a single LESS batch
+		.pipe(plugins.less({
+			paths: ['.'],
+			compress: false
+		}))
+
+		// Destination changes, manipulate internal URLs to point to public/
 		.pipe(plugins.cssUrlAdjuster({
 			prepend: config.cssUrlPrefix
 		}))
+
+		// Autoprefix
 		.pipe(plugins.autoprefixer(config.browserlist))
+
+		// Minify if not in debug mode
 		.pipe(plugins.if(!config.debug, plugins.minifyCss()))
+
+		// Rename with suffix
 		// .pipe(plugins.rename({suffix: '.min'}))
+
+		// Done
 		.pipe(gulp.dest(config.destination));
 });
 
@@ -79,8 +109,15 @@ gulp.task('clear-js', function (cb) {
 
 // Bower components
 gulp.task('js-vendor', function() {
-	return gulp.src(plugins.bowerFiles(['**/*.js']), { base: config.bowerComponentsPath })
+	var files = gulp.src(plugins.bowerFiles(['**/*.js']), { base: config.bowerComponentsPath });
+	return files
 		.pipe(plugins.plumber())
+
+		// Log files
+		.pipe(plugins.if(config.debug, plugins.debug({
+			title: 'File (vendor JS): '
+		})))
+
 		.pipe(plugins.concat('all.js', {newLine: ';'}))
 		.pipe(plugins.if(!config.debug, plugins.uglify()))
 		// .pipe(plugins.rename({suffix: '.min'}))
@@ -88,26 +125,20 @@ gulp.task('js-vendor', function() {
 });
 
 // Compile, uglify JS
-gulp.task('js', ['clear-js', 'js-vendor'], function () {
+gulp.task('js', function () {
 	var files = gulp.src(config.source.js);
 	return files
 		.pipe(plugins.plumber())
+
+		// Log files
+		.pipe(plugins.if(config.debug, plugins.debug({
+			title: 'File (JS): '
+		})))
+
 		.pipe(plugins.concat('vendor.js', {newLine: ';'}))
 		.pipe(plugins.if(!config.debug, plugins.uglify()))
 		// .pipe(plugins.rename({suffix: '.min'}))
 		.pipe(gulp.dest(config.destination));
-});
-
-// Watch for changes, recompile when needed
-gulp.task('watch', function () {
-	for (var ext in config.source) {
-		(function () {
-			var e = ext;
-			plugins.watch(config.source[e], plugins.batch(function () {
-				gulp.start(e);
-			}));
-		})()
-	}
 });
 
 
@@ -116,22 +147,35 @@ gulp.task('watch', function () {
 // SHORTHANDS
 //
 
-// Build all
-gulp.task('build', function () {
-	gulp.start('css', 'js');
-});
-
 // Clean all
-gulp.task('clear', function () {
+gulp.task('clear', [], function () {
 	gulp.start('clear-css', 'clear-js');
 });
 
-// Build all and start watching for changes
-gulp.task('build-and-watch', ['build'], function () {
-	gulp.start('watch');
+// Build all
+gulp.task('build', ['clear'], function () {
+	gulp.start('css-vendor', 'css', 'js-vendor', 'js');
+});
+
+// Watch for changes, recompile when needed
+gulp.task('watch', ['build'], function () {
+	for (var ext in config.source) {
+		(function () {
+			var e = ext;
+
+			// plugins.watch(config.source[e], plugins.batch(function () {
+			// 	gulp.start(e);
+			// }));
+
+			plugins.watch(config.source[e], function () {
+				gulp.start(e);
+			});
+
+		})()
+	}
 });
 
 // Default
 gulp.task('default', [], function () {
-	gulp.start('build-and-watch');
+	gulp.start('watch');
 });
